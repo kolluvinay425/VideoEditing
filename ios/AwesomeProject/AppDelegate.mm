9c26c798ee379c94d14973ef9d87887bfdc59a9c
Vinay <kolluvinay425@gmail.com>
2025-03-21 15:50:37 +0100

    debug

--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"  // Import RNSplashScreen
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,11 +8,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"AwesomeProject";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+  [RNSplashScreen show];  // Show splash screen after React Native initialization
+
+  return result;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
